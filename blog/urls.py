26e47a849f0f08db9120c8c01ca1d0d4f250fc8a
muchenzino <muche.borec@seznam.cz>
2024-01-30 10:39:44 +0100

    test6

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.models_list),
+    path('time', views.colorful_time),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+import random
 
 # Create your views here.
 
 def models_list(request):
-    return HttpResponse("Testing of views")
+    return HttpResponse("ČAU KERES")
+
+
+
+def colorful_time(request):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    color_code = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+    response_html = f"<p style='color: {color_code}; font-size: 24px;'>Aktuální čas: {current_time}</p>"
+
+    return HttpResponse(response_html)
+
