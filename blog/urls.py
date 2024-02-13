b948ca85bf5de398fa83f552295a9c582f73c5ec
muchenzino <muche.borec@seznam.cz>
2024-02-13 11:26:32 +0100

    test7

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('time', views.colorful_time),
+    path('cisla', views.generate_random_number),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 import random
+import math
+import hashlib
+import secrets
+
 
 # Create your views here.
 
@@ -22,3 +26,30 @@ def colorful_time(request):
 
     return HttpResponse(response_html)
 
+def random_predicted_number(secret_number):
+    # Převedení tajného čísla na řetězec
+    secret_str = str(secret_number)
+
+    # Hashování tajného čísla
+    hash_value = hashlib.sha256(secret_str.encode()).hexdigest()
+
+    # Konverze hash hodnoty na číslo v rozsahu 1-100
+    hash_number = int(hash_value, 16)
+    scaled_number = hash_number % 100 + 1
+
+    # Zvýšení čísla o 16 a zajištění, že zůstane v rozsahu 1-100
+    generated_number = (scaled_number + 16) % 100
+    if generated_number == 0:
+        generated_number = 100
+
+    return generated_number
+
+def generate_random_number(request):
+    # Tajné číslo, které budeme používat pro generování náhodných čísel
+    secret_number = 16  # Zde vložte vaše tajné číslo
+
+    # Generování předpovídaného čísla na základě tajného čísla
+    predicted_number = random_predicted_number(secret_number)
+
+    # Vracení výsledného čísla v HttpResponse
+    return HttpResponse(f"Vaše tajné náhodné číslo v rozsahu 1-100: {predicted_number}")
